79e82edabe3c4b58c486634897bd62e024c651bb
Mo <52931433+mscybr@users.noreply.github.com>
2023-09-17 08:17:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,99 +3,148 @@ const qrimage = require("qr-image");
 const express = require("express");
 const app = express();
 const port = 3000;
+const accounts = {};
 
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
-const client = new Client({
-  authStrategy: new LocalAuth(),
-});
-let qrr = "";
-let refreshes = 0;
-client.on("qr", (qr) => {
-  //   qrcode.generate(qr, { small: true });
-  qrr = qr;
-  console.log("qr");
-  refreshes++;
-});
+const e = require("express");
 
-client.initialize();
+function initialize_client(token, cb_ready, cb_auth, cb_no_auth) {
+  // before firing this function check to see if the account obj prop exsists or not
+  // let path = "./webjs_auth/sesssion-" + token;
+  let client = new Client({
+    restartOnAuthFail: true,
+    authStrategy: new LocalAuth({ clientId: token }),
+    puppeteer: {
+      args: [
+        "--no-sandbox",
+        // "--disable-setuid-sandbox",
+        // "--disable-dev-shm-usage",
+        // "--disable-accelerated-2d-canvas",
+        // "--no-first-run",
+        // "--no-zygote",
+        // "--disable-gpu",
+      ],
+    },
+  });
+  accounts[token].client = client;
+  accounts[token].qrr = "";
+  accounts[token].refreshes = 0;
+  client.on("qr", (qr) => {
+    accounts[token].qrr = qr;
+    accounts[token].refreshes++;
+    console.log("qr");
+    cb_no_auth();
+  });
 
-client.on("ready", () => {
-  console.log("Client is ready!");
-});
+  client.initialize();
 
-app.get("/get_refreshes", (req, res) => {
-  res.send(`${refreshes}`);
+  client.on("ready", () => {
+    console.log("Client is ready!");
+    cb_ready();
+  });
+
+  client.on("authenticated", () => {
+    console.log("Client is authed!");
+    cb_auth();
+  });
+}
+
+app.get("/get_refreshes/:token", (req, res) => {
+  res.send(`${accounts[req.params.token].refreshes}`);
 });
-app.get("/get_qr", (req, res) => {
-  res.header("Content-Type: image/png");
-  let img = qrimage.imageSync(qrr, { type: "png" });
-  //   res.set("Content-Type", "image/png");
-  //   res.send(img);
-  res.send(`
+app.get("/get_qr/:token", (req, res) => {
+  function connected() {
+    res.send(`
     <html>
     <body>
-    <img src="data:image/png;base64,${Buffer.from(img).toString("base64")}">
-    <script>
-    let refreshes = ${refreshes};
-    setInterval(()=>{
-        fetch("/get_refreshes").then((r)=>{ 
-            r.text().then((txt)=>{
-                if (txt.trim() != refreshes){
-                    location.href=location.href
-                }
-            })
-            
-        })
-    },1000)
-    
-    </script>
+    <p>already connected</p>
     </body>
     </html>
 `);
+  }
+  function not_connected() {
+    res.header("Content-Type: image/png");
+    let img = qrimage.imageSync(accounts[req.params.token].qrr, {
+      type: "png",
+    });
+    res.send(`
+  <html>
+  <body>
+  <img src="data:image/png;base64,${Buffer.from(img).toString("base64")}">
+  <script>
+  let refreshes = ${accounts[req.params.token].refreshes};
+  setInterval(()=>{
+      fetch("/get_refreshes").then((r)=>{
+          r.text().then((txt)=>{
+              if (txt.trim() != refreshes){
+                  location.href=location.href
+              }
+          })
+      })
+  },1000)
+  </script>
+  </body>
+  </html>
+`);
+  }
+  if (accounts[req.params.token] == null) {
+    accounts[req.params.token] = {};
+    initialize_client(req.params.token, connected, connected, not_connected);
+  } else {
+    accounts[req.params.token].client.getState().then((state) => {
+      if (state == "CONNECTED") {
+        connected();
+      } else {
+        not_connected();
+      }
+    });
+  }
 });
 
-//   res.send(img);
-app.get("/send_message", (req, res) => {
-  //   res.send(qr);
-  //   console.log();
+app.get("/send_message/:token", (req, res) => {
   if (req.query.number && req.query.image_url) {
-    client
-      .getState()
-      .then((state) => {
-        if (state == "CONNECTED") {
-          const number = req.query.number;
-          const text = req.query.image_url;
-          const chatId = number.substring(1) + "@c.us";
-          if (req.query.is_text == null) {
-            // sending media
-            const media = MessageMedia.fromUrl(text).then((media) => {
+    if (accounts[req.params.token] != null) {
+      let client = accounts[req.params.token].client;
+      client
+        .getState()
+        .then((state) => {
+          if (state == "CONNECTED") {
+            const number = req.query.number;
+            const text = req.query.image_url;
+            const chatId = number.substring(1) + "@c.us";
+            if (req.query.is_text == null) {
+              // sending media
+              const media = MessageMedia.fromUrl(text).then((media) => {
+                client
+                  .sendMessage(chatId, media)
+                  .then(() => {
+                    res.send("sent");
+                  })
+                  .catch(() => {
+                    res.send("not sent");
+                  });
+              });
+            } else {
+              // sending text
               client
-                .sendMessage(chatId, media)
+                .sendMessage(chatId, text)
                 .then(() => {
                   res.send("sent");
                 })
                 .catch(() => {
                   res.send("not sent");
                 });
-            });
+            }
           } else {
-            // sending text
-            client
-              .sendMessage(chatId, text)
-              .then(() => {
-                res.send("sent");
-              })
-              .catch(() => {
-                res.send("not sent");
-              });
+            res.send("not connected");
           }
-        } else {
-          res.send("not connected");
-        }
-      })
-      .catch(() => {
-        res.send("not sent");
-      });
+        })
+        .catch(() => {
+          res.send("not sent");
+        });
+    } else {
+      res.send("not started the client");
+    }
   }
 });
 
